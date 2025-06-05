aae97ab0182b2aa655558f822702dddc073249dd
Ibrar Ahmad <90978293+IbrarAhmad969@users.noreply.github.com>
2025-06-05 18:18:28 +0500

    JWT Tokens and User Model

---
+++ src/models/video.model.js
@@ -0,0 +1,52 @@
+import mongoose, { mongo, Schema } from "mongoose";
+
+//for watch history we will import paginate pkg
+
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+// use its plugin to use it ... 
+
+const videoSchema = new Schema({
+
+    videoFile: {
+        type: String, // cloudinary url
+        required: true,
+    },
+    thumbnail: {
+        type: String, // cloudinary url
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String, // cloudinary url
+        required: true,
+    },
+    description: {
+        type: Number, // cloudinary url
+        required: true,
+    },
+    views: {
+        type: Number,
+        default: 0,
+    },
+    isPublished: {
+        type: Boolean,
+        default: true,
+
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    }
+
+}, { timestamps: true })
+
+videoSchema.plugin(mongooseAggregatePaginate); //this is advance thing.
+
+export const Video = mongoose.model("Video", videoSchema);
